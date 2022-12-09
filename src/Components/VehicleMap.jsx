bab5422e23c7faf1560576d2a4bcfda71cfa8538
Omkar-Chaskar <Omkarchaskar1402@gmail.com>
2022-12-09 17:08:07 +0530

    feat : connect react app with firebase
    Firebase is connected with react app.
    but some bugs are still there

--- src/Components/VehicleMap.jsx
+++ src/Components/VehicleMap.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import startFirebase from "../firebase/config";
+import { useEffect } from "react";
+import { useVehicle } from "../Context/vehicleProvider";
+import { getDocs, collection } from 'firebase/firestore';
 
 function VehicleMap() {
+  const { vehicleState } = useVehicle();
+
+  const db = startFirebase();
+
+  useEffect(()=>{
+
+    const getUser =async () => {
+      // const userCollection = collection(db, `/${vehicleState[0].id}/${vehicleState[0].registrationNumber}/location`)
+      const userCollection = collection(db,`${vehicleState[0].id}-${vehicleState[0].registrationNumber}`)
+      const data = await getDocs(userCollection);
+      console.log(data);
+    }
+
+    vehicleState.length > 0 && getUser()
+  },[db,vehicleState])
+
   return (
     <></>
   )
